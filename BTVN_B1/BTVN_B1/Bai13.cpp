8945f5cf130ba965a4f7b43cee6ed22d7c480cb2
tuan anh <ttran8770@gmail.com>
2024-07-28 17:40:09 +0700

    add bài 14

--- BTVN_B1/BTVN_B1/Bai13.cpp
+++ BTVN_B1/BTVN_B1/Bai13.cpp
@@ -1,35 +1,35 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main() {
-    const int SIZE = 5; // Kích thước mảng
-    int a[SIZE], b[SIZE];
-
-    // Nhập mảng a
-    cout << "Nhap phan tu mang a: " << endl;
-    for (int i = 0; i < SIZE; i++) {
-        cout << "a[" << i << "] = ";
-        cin >> a[i];
-    }
-
-    // Nhập mảng b
-    cout << "Nhap phan tu mang b: " << endl;
-    for (int i = 0; i < SIZE; i++) {
-        cout << "b[" << i << "] = ";
-        cin >> b[i];
-    }
-
-    // Tính và xuất kết quả phép chia
-    cout << "Ket qua phep chia a[i] / b[i]: " << endl;
-    for (int i = 0; i < SIZE; i++) {
-        if (b[i] == 0) {
-            cout << "Khong the chia cho 0 tai chi so " << i << endl;
-        }
-        else {
-            float result = static_cast<float>(a[i]) / b[i];
-            cout << "a[" << i << "] / b[" << i << "] = " << result << endl;
-        }
-    }
-
-    return 0;
-}
+//int main() {
+//    const int SIZE = 5; // Kích thước mảng
+//    int a[SIZE], b[SIZE];
+//
+//    // Nhập mảng a
+//    cout << "Nhap phan tu mang a: " << endl;
+//    for (int i = 0; i < SIZE; i++) {
+//        cout << "a[" << i << "] = ";
+//        cin >> a[i];
+//    }
+//
+//    // Nhập mảng b
+//    cout << "Nhap phan tu mang b: " << endl;
+//    for (int i = 0; i < SIZE; i++) {
+//        cout << "b[" << i << "] = ";
+//        cin >> b[i];
+//    }
+//
+//    // Tính và xuất kết quả phép chia
+//    cout << "Ket qua phep chia a[i] / b[i]: " << endl;
+//    for (int i = 0; i < SIZE; i++) {
+//        if (b[i] == 0) {
+//            cout << "Khong the chia cho 0 tai chi so " << i << endl;
+//        }
+//        else {
+//            float result = static_cast<float>(a[i]) / b[i];
+//            cout << "a[" << i << "] / b[" << i << "] = " << result << endl;
+//        }
+//    }
+//
+//    return 0;
+//}
