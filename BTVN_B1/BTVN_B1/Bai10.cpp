d37a0f92a8eb903ce0a0d8334452e8205601b34f
tuan anh <ttran8770@gmail.com>
2024-08-06 14:33:03 +0700

    update bai10_B1.cpp va hinh anh ket qua, debug Bai10 buoi 1

--- BTVN_B1/BTVN_B1/Bai10.cpp
+++ BTVN_B1/BTVN_B1/Bai10.cpp
@@ -38,24 +38,24 @@ void chuyenKyTuDauCuaTuThanhChuHoa(char* st) {
     }
 }
 
-//int main() {
-//    char st[100];
-//
-//    cout << "Nhap vao chuoi: ";
-//    cin.getline(st, 100); // Nhap chuoi co dau cach
-//
-//    // a. Xuat gia tri tung ky tu
-//    inKyTu(st);
-//
-//    // b. Chuyen tat ca ky tu thanh chu hoa
-//    chuyenTatCaThanhChuHoa(st);
-//    cout << "Chuoi sau khi chuyen tat ca ky tu thanh chu hoa: " << st << endl;
-//
-//    // c. Khoi phuc chuoi goc de thuc hien yeu cau c
-//    cout << "Nhap lai chuoi de chuyen ky tu dau moi tu thanh chu hoa: ";
-//    cin.getline(st, 100);
-//    chuyenKyTuDauCuaTuThanhChuHoa(st);
-//    cout << "Chuoi sau khi chuyen ky tu dau moi tu thanh chu hoa: " << st << endl;
-//
-//    return 0;
-//}
+int main() {
+    char st[100];
+
+    cout << "Nhap vao chuoi: ";
+    cin.getline(st, 100); // Nhap chuoi co dau cach
+
+    // a. Xuat gia tri tung ky tu
+    inKyTu(st);
+
+    // b. Chuyen tat ca ky tu thanh chu hoa
+    chuyenTatCaThanhChuHoa(st);
+    cout << "Chuoi sau khi chuyen tat ca ky tu thanh chu hoa: " << st << endl;
+
+    // c. Khoi phuc chuoi goc de thuc hien yeu cau c
+    cout << "Nhap lai chuoi de chuyen ky tu dau moi tu thanh chu hoa: ";
+    cin.getline(st, 100);
+    chuyenKyTuDauCuaTuThanhChuHoa(st);
+    cout << "Chuoi sau khi chuyen ky tu dau moi tu thanh chu hoa: " << st << endl;
+
+    return 0;
+}
